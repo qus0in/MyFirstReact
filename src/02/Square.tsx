bf2fde104499e3d150aa702fb8b2562f0b7b6879
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2022-07-25 06:52:35 +0000

    사용자와 상호작용하는 컴포넌트 만들기

--- src/02/Square.tsx
+++ src/02/Square.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 
-class Square extends React.Component<{ value: number }> {
+class Square extends React.Component<{ value: number }, { value: any }> {
+  constructor(props: any) {
+    super(props)
+    this.state = { value: null, }
+  }
+
   render() {
+    // return (
+    //   <button className="square" onClick={
+    //     // function() { console.log('click') }
+    //     () => console.log('click') // arrow function
+    //   }>
+    //     {this.props.value}
+    //   </button>
+    // )
     return (
-      <button className="square">
-        {this.props.value}
+      <button
+        className="square"
+        onClick={() => this.setState({ 'value': 'X' })
+        }>
+        {this.state.value}
       </button>
     )
   }
