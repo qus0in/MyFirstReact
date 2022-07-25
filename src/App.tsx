afd723db89eaf462b99a695d98585c27e7c3f561
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2022-07-25 06:23:13 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import './App.css'
+import { ShoppingList } from './ShoppingList'
+
+export default function App() {
+  // return <div className="app">hello world</div>;
+  return <ShoppingList name='Sad Cat'/>
+}
