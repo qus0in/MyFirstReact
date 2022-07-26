36820d9eb13cf5f4f7127b2af7a28c6194ecf335
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2022-07-26 09:16:28 +0000

    다시 State 끌어올리기

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import './App.css'
 // import { ShoppingList } from './01/ShoppingList'
 // import Board from './02/Board'
 // import Board from './03/Board'
-import Board from './04/Board'
+// import Board from './04/Board'
+import Game from './05/Game'
 
 export default function App() {
   // return <div className="app">hello world</div>;
   // return <ShoppingList name='Sad Cat' />
-  return <Board />
+  // return <Board />
+  return <Game />
 }
