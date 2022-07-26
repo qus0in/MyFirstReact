5e52aa346b232e184f6be48eba8c5cb8afb50aad
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2022-07-26 09:30:45 +0000

    시간여행 구현하기

--- src/05/Game.tsx
+++ src/05/Game.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Board from './Board'
 
 class Game extends React.Component<{}, {
-  history: any, xIsNext: boolean
+  history: any, xIsNext: boolean, stepNumber: number
 }> {
   constructor(props: any) {
     super(props)
@@ -10,12 +10,14 @@ class Game extends React.Component<{}, {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     }
   }
 
   handleClick(i: number) {
-    const history = this.state.history
+    // const history = this.state.history
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -24,17 +26,22 @@ class Game extends React.Component<{}, {
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
       history: history.concat([{ squares }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     })
   }
 
-  jumpTo(move: any) {
-    /* TODO */
+  jumpTo(step: any) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    })
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1]
+    // const current = history[history.length - 1]
+    const current = history[this.state.stepNumber]
     const winner = calculateWinner(current.squares)
 
     const moves = history.map((step: any, move: any) => {
