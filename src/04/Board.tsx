142b7175dc892c584d9dc64318c5eb63f14b9f9a
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2022-07-26 07:05:03 +0000

    승자 결정하기

--- src/04/Board.tsx
+++ src/04/Board.tsx
@@ -15,6 +15,9 @@ class Board extends React.Component<{},
     // 배열의 복사본 생성
     // console.log(i)
     const squares = this.state.squares.slice()
+    if (calculateWinner(squares) || squares[i]) {
+      return // 결과가 났거나 이미 차있는 박스라면 작동 무시
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({ squares: squares, xIsNext: !this.state.xIsNext, })
   }
@@ -29,7 +32,14 @@ class Board extends React.Component<{},
 
   render() {
     // const status = 'Next player: X';
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    // const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -52,6 +62,21 @@ class Board extends React.Component<{},
       </div>
     );
   }
+}
+
+function calculateWinner(squares: Array<any>) {
+  const lines = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+    [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    // 1) 첫번째 자리가 null이 아님 2) 첫번째 자리가 두.세번째 자리와 일치
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default Board
